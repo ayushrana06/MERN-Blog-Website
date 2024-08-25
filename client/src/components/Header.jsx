a5698f5ece4e86fa57378fb381e2e9be5163bd59
ayushrana06 <ayush.rana0603@gmail.com>
2024-08-25 13:52:30 +0530

    complete search page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, TextInput } from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon, FaSun} from 'react-icons/fa';
 import {useSelector, useDispatch} from 'react-redux';
@@ -9,9 +9,21 @@ import { useEffect, useState } from 'react';
 
 export default function Header() {
     const path = useLocation().pathname;
+    const location = useLocation();
     const dispatch = useDispatch();
+    const navigate = useNavigate();
     const {currentUser} = useSelector((state) => state.user);
     const {theme} = useSelector((state) => state.theme);
+    const [searchTerm, setSearchTerm] = useState('');
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if(searchTermFromUrl){
+            setSearchTerm(searchTermFromUrl); 
+        }
+    }, [location.search]);
+
     const handleSignout = async () => {
         try{
             const res = await fetch('/api/user/signout',{
@@ -30,18 +42,28 @@ export default function Header() {
         }
     };
 
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+    };
+
     return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
             <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Ayush's</span>
             Blog
         </Link>
-        <form>
+        <form onSubmit={handleSubmit}>
             <TextInput
                 type='text'
                 placeholder='Search...'
                 rightIcon={AiOutlineSearch}
                 className='hidden lg:inline'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
             />
         </form>
         <Button className='w-12 h-10 lg:hidden' color='gray' pill>
